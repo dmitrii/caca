4233290ca17dd170809cef3ed645721ed5eadfda
Dmitrii Calzago <dcalzago@gmail.com>
2025-12-17 13:55:00 -0800

    chore: export loaders from package

--- caca/loaders/__init__.py
+++ caca/loaders/__init__.py
@@ -1,1 +1,15 @@
 # PURPOSE: Data file loaders for plans, profiles, costs, and parameters
+
+from caca.loaders.plan_loader import load_plan_yaml
+from caca.loaders.profile_loader import load_profile_yaml
+from caca.loaders.costs_loader import load_costs_yaml
+from caca.loaders.params_loader import load_params_yaml
+from caca.loaders.run_config_loader import load_run_config
+
+__all__ = [
+    "load_plan_yaml",
+    "load_profile_yaml",
+    "load_costs_yaml",
+    "load_params_yaml",
+    "load_run_config",
+]
